42b91d90c9325f60c324f2f1b464e2d45ceff7a1
soffqes <119006275+soffqes@users.noreply.github.com>
2023-06-02 13:04:48 +0300

    Add files via upload

---
+++ ToursAppStarovoytova/ToursAppStarovoytova/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToursAppStarovoytova
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
